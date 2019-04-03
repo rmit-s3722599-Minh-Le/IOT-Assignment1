72535b6c9e7b40fbb314adbf36c1fb1121b522bb
s3722599 <s3722599@student.rmit.edu.au>
2019-04-03 13:02:31 +1100

    json Parse, gets temp and humid

---
+++ monitorAndNotify.py
@@ -0,0 +1,27 @@
+#imports
+import json
+from sense_hat import SenseHat
+
+#readfile
+with open('config.json', 'r') as myfile:
+    data = myfile.read()
+
+#parsefile
+rangesData = json.loads(data)
+
+#place data (range) from config.json to local variables
+max_temp = rangesData['max_temp']
+min_temp = rangesData['min_temp']
+max_humid = rangesData['max_humid']
+min_humid = rangesData['min_humid']
+
+
+#sensehat
+
+sense = SenseHat()
+sense.clear()
+
+#gets temp and humid
+temp  = sense.get_temperature()
+temp_humid = sense.get_temperature_from_humidity()
+
